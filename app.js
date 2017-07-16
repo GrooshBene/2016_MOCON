46ff431df6aff1c6fae1b6055a02cf8252011d03
GrooshBene <wltn000129@gmail.com>
2017-07-16 19:57:27 +0900

    Schema Defined, Module Files Created

--- app.js
+++ app.js
@@ -4,12 +4,53 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var randomString = require('randomString');
+var schema = mongoose.Schema;
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
 
 var app = express();
-var mongoose = require('mongoose');
+var UserSchema = new schema({
+	_id : String,
+	name : String,
+	address : String,
+	bucket : {
+		type : String,
+		ref : 'buckets'
+	},
+	purchase_history : [{
+		type : String,
+		ref : 'buckets'
+	}],
+	payment_info : Object
+});
+
+var BucketSchema = new schema({
+	_id : String,
+	content : [{
+		type : String,
+		ref : 'goods'
+	}]
+});
+
+var GoodsSchema = new schema({
+	_id : String,
+	name : String,
+	thumbnail : String,
+	description : String,
+	price : Number
+});
+
+var TruckSchema = new schema({
+	_id : String,
+	goods_type : String,
+
+})
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -22,7 +63,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+
+require('./routes/auth.js');
+require('./routes/bucket.js');
+require('./routes/gps.js');
+require('./routes/market.js');
+require('./routes/pay.js');
+require('./routes/truck.js');
+require('./routes/users.js');
+
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {

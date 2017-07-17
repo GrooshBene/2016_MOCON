63e77451374d861461e703b56217612efcb8c61c
GrooshBene <wltn000129@gmail.com>
2017-07-17 12:28:44 +0900

    gps.js gps-tracker module updated

--- routes/gps.js
+++ routes/gps.js
@@ -1,4 +1,13 @@
 function init(app, Truck, User){
+	var gps = require('gps-tracking');
+	var options = {
+		'debug' : true,
+		'port' : 8090,
+		'device_adapter' : "TK103"
+	}
 
+	var server = gps.server(options, function(device_id, msg_parts){
+		//device.on
+	})
 }
 module.exports = init;

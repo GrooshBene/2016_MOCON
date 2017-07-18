71594cb1ba81e149e5bca7411f927a4b70768a86
GrooshBene <wltn000129@gmail.com>
2017-07-18 20:16:36 +0900

    auth.js token authenticate error fixed

--- routes/auth.js
+++ routes/auth.js
@@ -24,7 +24,13 @@ function init(app, User, randomString){
 			}
 			if(!user){
 				user = new User({
-					//plz add user info
+					_id : profile.id,
+					name : profile.displayName,
+					address : "",
+					bucket : "",
+					purchase_history : [],
+					payment_info : {},
+					location : {}
 				});
 				user.save(function(err){
 					if(err) console.log(err);
@@ -42,7 +48,13 @@ function init(app, User, randomString){
 	app.get('/auth/facebook/token', passport.authenticate('facebook-token'), function(req, res){
 		console.log("user token : " + req.param('access_token'));
 		if(req.user){
-			res.send(200, response);
+			User.findOneAndUpdate({_id : req.user.id}, {address : req.param('address')}, function(err, result){
+				if(err){
+					console.log(err);
+					res.send(401, "/auth/facebook/token DB Update Error");
+				}
+				res.send(200, result);
+			})
 		}
 		else if(!req.user){
 			res.send(401, "Can't find User on Facebook. It May Be Unusable");
